d48f60a775ca3be3a06f028aebcc4a2d67219515
Dimitris Kougioumtzis <dimkoug@gmail.com>
2021-01-23 12:16:30 +0200

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,34 @@
 from django import forms
+from django.utils.html import format_html
 from django.core.exceptions import ValidationError
+from django.forms.models import BaseInlineFormSet
+from adminsortable2.admin import CustomInlineFormSet
+from django.contrib.admin.widgets import AdminFileWidget
+from django.utils.safestring import mark_safe
+from django.forms import ModelForm
+from sorl.thumbnail import get_thumbnail
+
+
+def unique_field_formset(field_name):
+    from django.forms.models import BaseInlineFormSet
+    from adminsortable2.admin import CustomInlineFormSet
+    class UniqueFieldFormSet(CustomInlineFormSet, BaseInlineFormSet):
+        def clean(self):
+            if any(self.errors):
+                # Don't bother validating the formset unless each form is valid on its own
+                return
+            values = set()
+            for form in self.forms:
+                if form.cleaned_data:
+                    value = form.cleaned_data[field_name]
+                    if value in values:
+                        raise forms.ValidationError('Duplicate values for "%s" are not allowed.' % field_name)
+                    values.add(value)
+    return UniqueFieldFormSet
+
+
+class ShortableFormSet(CustomInlineFormSet, BaseInlineFormSet):
+    pass
 
 
 class SlugValidator:
@@ -14,6 +43,17 @@ class SlugValidator:
         return slug
 
 
+
+class AdminImageWidget(AdminFileWidget):
+  def render(self, name, value, attrs=None, renderer=None):
+    output = []
+    if value and getattr(value, "url", None):
+      t = get_thumbnail(value,'80x80')
+      output.append('<img src="{}">'.format(t.url))
+    output.append(super(AdminImageWidget, self).render(name, value, attrs))
+    return format_html(u''.join(output))
+
+
 class BootstrapForm:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -24,3 +64,4 @@ class BootstrapForm:
                 self.fields[field].widget.attrs.update({
                     'class': 'form-control'
                 })
+
