d2d93918ed12e7bb3126f4712bbdfa2c4f96ab31
Dimitris Kougioumtzis <dimkoug@gmail.com>
2021-01-23 12:17:55 +0200

    Update mixins.py

--- core/mixins.py
+++ core/mixins.py
@@ -2,21 +2,37 @@ from django.conf import settings
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 
 
-class ProtectedViewMixin:
-
-    def dispatch(self, *args, **kwargs):
-        if self.request.user.is_anonymous:
-            return redirect(reverse_lazy('login') + '?next={}'.format(self.request.path))
-        return super().dispatch(*args, **kwargs)
+class DynamicTemplateMixin:
+    '''
+    Create template dynamically
+    '''
+    def get_template_names(self):
+        model_name = self.model.__name__.lower()
+        view_template = ""
+        app = self.model._meta.app_label
+        if not hasattr(self, 'template'):
+            raise AttributeError(
+                "Add template attribute to your {}  for example if list view"
+                " then add template='list' appropriate values"
+                " list,detail,form, confirm_delete".format(self.__class__.__name__)
+            )
+        view_template =  "{}/{}_{}.html".format(app,model_name, self.template)
+        return [view_template]
 
 
 class ModelMixin:
+    '''
+    Add  app and model to context
+    '''
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['model'] = self.model
+        context['model_name'] = self.model.__name__.lower()
+        context['app_name'] = self.model._meta.app_label
         return context
 
 
@@ -40,7 +56,13 @@ class SuccessUrlMixin:
 
 class MessageMixin:
     def form_valid(self, form):
-        form.save()
+        obj = form.save()
+        model_name = self.model.__name__.lower()
+        app = self.model._meta.app_label
+        if 'new' in self.request.POST:
+            return redirect(reverse_lazy("{}:{}-create".format(app, model_name)))
+        if 'continue' in self.request.POST:
+            return redirect(reverse_lazy("{}:{}-update".format(app, model_name), kwargs={"pk":obj.pk}))
         messages.success(
             self.request, 'Your {} was proccesed successfully!'.format(
                 self.model.__name__))
@@ -58,3 +80,10 @@ class PassRequestToFormViewMixin:
         kwargs = super().get_form_kwargs()
         kwargs['request'] = self.request
         return kwargs
+
+class BaseViewMixin(LoginRequiredMixin, DynamicTemplateMixin,
+                    ModelMixin):
+    pass
+
+class FormViewMixin(BaseViewMixin,SuccessUrlMixin,MessageMixin):
+    pass
