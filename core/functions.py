a7e59510d46085164cd514d8467f6b968496d2b6
Dimitris Kougioumtzis <dimkoug@gmail.com>
2023-01-05 17:23:17 +0200

    Update functions.py

--- core/functions.py
+++ core/functions.py
@@ -35,3 +35,30 @@ def create_query_string(request):
                 if value != '':
                     query_string += "&{}={}".format(key, value)
     return query_string
+
+
+def get_select_2_data(request):
+    # example b2buser
+    model_str = request.GET.get('model')
+    # example b2b
+    app_str = request.GET.get('app')
+    q_objects = Q()
+    d_objects = []
+    q = request.GET.get('q')
+    model = apps.get_model(app_label=app_str, model_name=model_str)
+    for f in  model._meta.get_fields():
+        print(f.__class__.__name__)
+        if f.__class__.__name__  in ['CharField', 'TextField']:
+            str_q = f"Q({f.name}__icontains=str({q}))"
+            print(str_q)
+            q_obj = eval(str_q)
+            print(q_obj)
+            q_objects |= q_obj
+
+    data = model.objects.filter(q_objects)
+    for d in data:
+        d_objects.append({
+            "id": d.pk,
+            "title": d.__str__()
+        })
+    return JsonResponse(d_objects,safe=False)
