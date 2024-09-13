cf30bf1a0530453b11d7043e080cf0ad75bdefde
Dimitris Kougioumtzis <dimkoug@gmail.com>
2024-09-13 13:33:21 +0300

    wip

--- project_name/api/viewsets.py
+++ project_name/api/viewsets.py
@@ -29,12 +29,18 @@ def current_user(request):
 class RegisterApi(generics.GenericAPIView):
     serializer_class = UserSerializer
 
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = (permissions.AllowAny,)
+
+        return super().get_permissions()
+
     def post(self, request, *args,  **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response({
-            "user": UserSerializer(user,context=self.get_serializer_context()).data,
+            "user": UserSerializerWithToken(user,context=self.get_serializer_context()).data,
             "message": "User Created Successfully.  Now perform Login to get your token",
         })
 
