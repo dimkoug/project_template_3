c40cb8334afa385d354cfcb80fb0246e98e9e865
Dimitris Kougioumtzis <dimkoug@gmail.com>
2025-01-28 11:22:25 +0200

    Update settings_local_sample.py

--- project_name/settings_local_sample.py
+++ project_name/settings_local_sample.py
@@ -101,7 +101,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': '',
 #         'USER': 'postgres',
 #         'PASSWORD': '',
